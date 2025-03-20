004d2a2bb2e3517eaeb5fab3fe35a782ad86427d
Giuseppe Argiolas <giuseppe_argiolas@outlook.it>
2025-03-20 22:20:53 +0100

    feat: Add DocFX project

--- CursorPagination/Application/Abstractions/Cursor.cs
+++ CursorPagination/Application/Abstractions/Cursor.cs
@@ -1,4 +1,6 @@
-﻿namespace CursorPagination.Application.Abstractions;
+﻿using CursorPagination.Application.Abstractions.Services;
+
+namespace CursorPagination.Application.Abstractions;
 
 /// <summary>
 /// Represents a cursor utilized in pagination for tracking position and metadata associated with a specific entity.
@@ -7,4 +9,4 @@
 /// <param name="LastId">The unique identifier of the most recent entity within the cursor context.</param>
 /// <param name="Entity">The entity object corresponding to the current position in the paginated set.</param>
 /// <param name="Position">The numerical position of the current entity relative to the dataset.</param>
-public sealed record Cursor<TEntity>(Guid LastId, TEntity Entity, int Position);
+public sealed record Cursor<TEntity>(Guid LastId, TEntity Entity, int Position) where TEntity : ICursorFilter;
